f9b9781a1a08be83d714e7985fb2d0f5f1a71a39
Igor <igor.lischewski@web.de>
2021-02-12 10:30:10 +0100

    Array_Basic

--- script_01.js
+++ script_01.js
@@ -1,10 +1,36 @@
 
 /*** 1.0 Arrays */
 
+/*
+let arr;
+arr = new Array(); // Konstruktor
+arr = []; // Literal
+arr = [2,6,7,12];
+arr = [true,false];
+arr = ["Ich","bin","Drazen"];
+
+ausgabe(arr);
+ausgabe(arr[0]);
+ausgabe(arr[2]);
+ausgabe(arr.length);
+*/
 
 
 
 
+//"Ich bin Drazen."
+//ausgabe(getSentence("Ich","bin","Drazen","Savi")); // Test
+function getSentence(word1,word2,word3,word4) {
+
+    let gap = " ";
+    let punct = ".";
+    let str = word1 + gap + 
+              word2 + gap + 
+              word3 + gap + 
+              word4 + 
+              punct;
+    return str;
+}
 
 // Modul: Ausgabe in Konsole | Test:
 //ausgabe("hi");

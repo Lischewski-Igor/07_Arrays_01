913810e050978ff0552cc374dbb5e03d900bc274
Igor <igor.lischewski@web.de>
2021-02-12 11:43:37 +0100

    For-Schleife

--- script_01.js
+++ script_01.js
@@ -1,7 +1,7 @@
 
 /*** 1.0 Arrays */
 
-/*
+/* 1. Array
 let arr;
 arr = new Array(); // Konstruktor
 arr = []; // Literal
@@ -15,13 +15,43 @@ ausgabe(arr[2]);
 ausgabe(arr.length);
 */
 
+/* 1a. Schleifen (for-schleifen) */
+let array = ["Ich","bin","Drazen","Savi"];
+//ausgabe(arr);
+// i 0,1,2,3 ... n
+/*
+for (let i = 0; i < 10; i++) {
+    ausgabe(i);  
+}
+*/
+
+// for (let i = 0; i < array.length; i++) {
+//     ausgabe(i + ":" + array[i]);  
+// }
 
 
+//*** 02a. Funktionalität mit Array */
+// Kritik|Überlegung
+// word1,word2, ...... word100 :: Semantisch Ordnung
+// arr[0],arr[1] ... arr[n] :: Numerische Ordnung (0,1,2,3 ....n)
+//let arr = ["Ich","bin","Drazen","Savi"];
+//ausgabe(getSentenceArr(arr)); // Test
+function getSentenceArr(arr) {
+    let gap = " ";
+    let punct = ".";
 
-//"Ich bin Drazen."
+    let str = arr[0] + gap + 
+              arr[1] + gap + 
+              arr[2] + gap + 
+              arr[3] + 
+              punct;
+
+    return str;
+}
+
+//*** 01. Funktionalität mit Einzelparametern */
 //ausgabe(getSentence("Ich","bin","Drazen","Savi")); // Test
 function getSentence(word1,word2,word3,word4) {
-
     let gap = " ";
     let punct = ".";
     let str = word1 + gap + 
